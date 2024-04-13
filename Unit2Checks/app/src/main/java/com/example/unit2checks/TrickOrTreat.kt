6f324ff80b5dd6abc903519e5ce30c8a37af7edd
Kostiantyn Mykhailenko <k.mykhailenko@dxc.com>
2024-04-13 19:32:09 +0300

    lambdas&function sugar

---
+++ Unit2Checks/app/src/main/java/com/example/unit2checks/TrickOrTreat.kt
@@ -0,0 +1,53 @@
+package com.example.unit2checks
+
+// lambdas experience
+//fun main() {
+//    val trickFunction = trick
+//    trickFunction()
+//    treat()
+//}
+
+val trick = {
+    println("No treats!")
+}
+
+val treat: () -> Unit = {
+    println("Have a treat!")
+}
+
+// function that returns a function
+fun main() {
+    val coins: (Int) -> String = {
+        "$it quarters"
+    }
+
+    val cupcake: (Int) ->String = {
+        "Have a cupcake!"
+    }
+
+    val treatFunction = trickOrTreat(false, { "$it quater" })
+    /*
+    * Trailing lambda syntax
+    * val treatFunction = trickOrTreat(false) { "$it quater" }
+    * */
+    val trickFunction = trickOrTreat(true, null)
+
+    treatFunction()
+    trickFunction()
+
+    repeat(4) {
+        treatFunction()
+    }
+}
+
+fun trickOrTreat(isTrick: Boolean, extraTreat: ((Int) -> String)?): () -> Unit {
+    if (isTrick) {
+        return trick
+    }
+
+    if (extraTreat != null) {
+        println(extraTreat(5))
+    }
+
+    return treat
+}
